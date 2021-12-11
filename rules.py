6fcbbdf3f815c30dfe3c6d2f728518a611b7dbc9
Tyler Hecht <50639776+RadiateGold@users.noreply.github.com>
2021-12-11 17:03:02 -0500

    Code actually returns things now

--- rules.py
+++ rules.py
@@ -12,12 +12,30 @@ with open('courses_list.json') as data_file:
 
 
 # main function
-def check_cs_requirements() -> str:
-    pass
+def check_cs_requirements(courses_taken: dict) -> dict:
+    fulfilled = {}
+    # university requirements
+    fulfilled["ENGL 110"] = check_course(courses_taken, "ENGL 110")
+    fulfilled["First year experience"] = check_FYS(courses_taken)
+    fulfilled["Discover learning experience"] = check_DLE(courses_taken)
+    fulfilled["Multicultural"] = check_multicultural(courses_taken)
+    for group in ["A", "B", "C", "D"]:
+        fulfilled["Group " + group] = check_group(courses_taken, group)
+    fulfilled["Capstone"] = check_capstone(courses_taken)
+    # major requirements
+    for course_number in ["108", "181", "210", "220", "260", "275", "303", "320", "361", "372"]:
+        course = "CISC " + course_number
+        fulfilled[course] = check_course(courses_taken, course)
+    fulfilled["MATH 205 or MATH 350"] = check_course(courses_taken, "MATH 205") or check_course(courses_taken, "MATH 350")
+    for course_number in ["210", "241", "242"]:
+        course = "MATH " + course_number
+        fulfilled[course] = check_course(courses_taken, course)
+    fulfilled["Capstone requirement"] = (check_course(courses_taken, "CISC 498") and check_course(courses_taken, "CISC 499")) or (check_course(courses_taken, "UNIV 401") and check_course(courses_taken, "UNIV 402"))
+    return fulfilled
     
 # helper functions
-def check_ENGL110(courses_taken: dict) -> bool:
-    return "ENGL 110" in courses_taken
+def check_course(courses_taken: dict, course: str) -> bool:
+    return course in courses_taken
 
 def check_FYS(courses_taken: dict) -> bool:
     fulfilled = False
@@ -51,8 +69,16 @@ def check_group(courses_taken: dict, group: str) -> bool:
                 credits += courses_taken[course]
     return credits >= 3
 
+def check_capstone(courses_taken: dict) -> bool:
+    credits = 0
+    for course in courses_taken:
+        if course in courses_list:
+            if courses_list[course]["capstone"]:
+                credits += courses_taken[course]
+    return credits >= 3
+
 # example of courses taken
 courses_taken = {"HIST 304": 4}
 
 # testing
-print(check_group(courses_taken, "B"))
+print(check_cs_requirements(courses_taken))
