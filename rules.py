46fd5de2e404531378cf95107e61ee2986ea1ca5
Tyler Hecht <50639776+RadiateGold@users.noreply.github.com>
2021-12-11 17:21:06 -0500

    Added more rules

--- rules.py
+++ rules.py
@@ -13,29 +13,46 @@ with open('courses_list.json') as data_file:
 
 # main function
 def check_cs_requirements(courses_taken: dict) -> dict:
+    # dictionary that says whether or not a requirement has been fulfilled
     fulfilled = {}
     # university requirements
-    fulfilled["ENGL 110"] = check_course(courses_taken, "ENGL 110")
+    fulfilled["ENGL 110"] = check_courses(courses_taken, ["ENGL 110"])
     fulfilled["First year experience"] = check_FYS(courses_taken)
     fulfilled["Discover learning experience"] = check_DLE(courses_taken)
     fulfilled["Multicultural"] = check_multicultural(courses_taken)
     for group in ["A", "B", "C", "D"]:
         fulfilled["Group " + group] = check_group(courses_taken, group)
     fulfilled["Capstone"] = check_capstone(courses_taken)
-    # major requirements
+    # general CISC courses
     for course_number in ["108", "181", "210", "220", "260", "275", "303", "320", "361", "372"]:
         course = "CISC " + course_number
-        fulfilled[course] = check_course(courses_taken, course)
-    fulfilled["MATH 205 or MATH 350"] = check_course(courses_taken, "MATH 205") or check_course(courses_taken, "MATH 350")
+        fulfilled[course] = check_courses(courses_taken, [course])
+    # stat sequence
+    fulfilled["MATH 205 or MATH 350"] = check_courses(courses_taken, ["MATH 205"]) or check_courses(courses_taken, ["MATH 350"])
+    # general MATH courses
     for course_number in ["210", "241", "242"]:
         course = "MATH " + course_number
-        fulfilled[course] = check_course(courses_taken, course)
-    fulfilled["Capstone requirement"] = (check_course(courses_taken, "CISC 498") and check_course(courses_taken, "CISC 499")) or (check_course(courses_taken, "UNIV 401") and check_course(courses_taken, "UNIV 402"))
+        fulfilled[course] = check_courses(courses_taken, [course])
+    # CS Capstone requirement
+    capstone_sequence_1 = check_courses(courses_taken, ["CISC 498", "CISC 499"])
+    capstone_sequence_2 = check_courses(courses_taken, ["UNIV 401", "UNIV 402"])
+    fulfilled["Capstone requirement"] = capstone_sequence_1 or capstone_sequence_2
+    # science courses
+    science_sequence_1 = check_courses(courses_taken, ["PHYS 207", "PHYS 227", "PHYS 208", "PHYS 228"])
+    science_sequence_2 = check_courses(courses_taken, ["CHEM 103", "CHEM 133", "CHEM 104", "CHEM 134"])
+    science_sequence_3 = check_courses(courses_taken, ["BISC 207", "BISC 208"])
+    science_sequence_4 = check_courses(courses_taken, ["GEOL 105", "GEOL 107", "GEOL 115"])
+    science_sequence_5 = check_courses(courses_taken, ["GEOL 107", "GEOL 110"])
+    fulfilled["Science courses"] = science_sequence_1 or science_sequence_2 or science_sequence_3 or science_sequence_4 or science_sequence_5 
+    # return the dictionary
     return fulfilled
     
 # helper functions
-def check_course(courses_taken: dict, course: str) -> bool:
-    return course in courses_taken
+def check_courses(courses_taken: dict, courses: [str]) -> bool:
+    fulfilled = True
+    for course in courses:
+        fulfilled = fulfilled and course in courses_taken
+    return fulfilled
 
 def check_FYS(courses_taken: dict) -> bool:
     fulfilled = False
@@ -78,7 +95,7 @@ def check_capstone(courses_taken: dict) -> bool:
     return credits >= 3
 
 # example of courses taken
-courses_taken = {"HIST 304": 4}
+courses_taken = {"CISC 108": 4}
 
 # testing
 print(check_cs_requirements(courses_taken))
